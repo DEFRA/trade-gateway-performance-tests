ea0b08b67d20e1d31b867ad810ce36e83b355a14
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2026-09-22 14:45:27 +0100

    CMDS-BAU: Update GitHub actions workflows (#3)
    * CMDS-BAU: Update GitHub actions workflows
    
    * CDMS-BAU: Run npm audit fix
    
    * CDMS-BAU: Merge main
    
    * CDMS-BAU: Update test configuration

--- src/config/environment.js
+++ src/config/environment.js
@@ -2,11 +2,11 @@ export const env = {
   // URL of token service, default local
   tokenUrl:
     !__ENV.ENVIRONMENT || __ENV.ENVIRONMENT === 'local'
-      ? 'http://localhost:3001/local/cognito/token'
+      ? 'http://localhost:8080/local/cognito/token'
       : `https://trade-gateway.${__ENV.ENVIRONMENT}.cdp-int.defra.cloud/local/cognito/token`,
   // URL of Trade Gateway service, default local
   serviceUrl:
     !__ENV.ENVIRONMENT || __ENV.ENVIRONMENT === 'local'
-      ? 'http://localhost:3001'
+      ? 'http://localhost:8080'
       : `https://trade-gateway.${__ENV.ENVIRONMENT}.cdp-int.defra.cloud`,
 };
